eead707ad67ecd0276f1f2ee8d6ddbd172a5827c
dsr <bdbcat@yahoo.com>
2021-06-03 21:42:09 -0400

    Remove invalid "KEEP" status tag on legacy oesenc charts.

--- src/eSENCChart.cpp
+++ src/eSENCChart.cpp
@@ -560,7 +560,7 @@ int oeuSENCChart::Init( const wxString& name, int init_flags )
     if( !wxFileName::FileExists( name ) )
         return PI_INIT_FAIL_REMOVE;
 
-    if(!processChartinfo( name, _T("KEEP") )){
+    if(!processChartinfo( name )){
         return PI_INIT_FAIL_REMOVE;
     }
     
@@ -648,7 +648,7 @@ int oeuEVCChart::Init( const wxString& name, int init_flags )
     if( !wxFileName::FileExists( name ) )
         return PI_INIT_FAIL_REMOVE;
 
-    if(!processChartinfo( name, _T("KEEP") )){
+    if(!processChartinfo( name )){
         return PI_INIT_FAIL_REMOVE;
     }
     
