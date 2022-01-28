836c074e16386d35d06bf30ee1ddf8cd602c0812
dsr <bdbcat@yahoo.com>
2022-01-28 09:55:37 -0500

    Test Android #4

--- ci/circleci-build-android.sh
+++ ci/circleci-build-android.sh
@@ -50,7 +50,15 @@ python3 -m pip install --user -q cmake
 cd $builddir
 
 sudo ln -sf /opt/android/android-ndk-* /opt/android/ndk
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake/$OCPN_TARGET-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake \
+  -DCMAKE_TOOLCHAIN_FILE=cmake/$OCPN_TARGET-toolchain.cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_SYSROOT=/opt/android/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/sysroot \
+  -DCMAKE_INCLUDE_PATH=/opt/android/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include \
+  -DCMAKE_LIBRARY_PATH=/opt/android/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/16 \
+  ..
+
+
 make VERBOSE=1
 
 if [ -d /ci-source ]; then sudo chown --reference=/ci-source -R . ../cache; fi
