84ca30af4268f97fd830f402d76ea29308ae2edb
dsr <bdbcat@yahoo.com>
2022-01-28 00:05:33 -0500

    Test Android #3

--- ci/circleci-build-android.sh
+++ ci/circleci-build-android.sh
@@ -31,7 +31,7 @@ exec > >(tee $builddir/build.log) 2>&1
 # The local container needs to access the cache directory
 test -d cache || sudo mkdir cache
 test -w cache || sudo chmod -R go+w cache || :
- 
+
 
 sudo apt -q update
 sudo apt install -q cmake git gettext
@@ -50,7 +50,7 @@ python3 -m pip install --user -q cmake
 cd $builddir
 
 sudo ln -sf /opt/android/android-ndk-* /opt/android/ndk
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake/$OCPN_TARGET-toolchain.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/$OCPN_TARGET-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
 make VERBOSE=1
 
 if [ -d /ci-source ]; then sudo chown --reference=/ci-source -R . ../cache; fi
