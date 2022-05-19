5a0f057aee2e03f4afd5adb251250568ef3f95bf
dsr <bdbcat@yahoo.com>
2022-05-18 21:30:08 -0400

    Correct fpr generation command string, Windows, adding trailing "\"" character

--- src/fpr.cpp
+++ src/fpr.cpp
@@ -56,7 +56,7 @@ void androidGetDeviceName()
 {
     if(!g_deviceInfo.Length())
         g_deviceInfo = callActivityMethod_vs("getDeviceInfo");
-    
+
     wxStringTokenizer tkz(g_deviceInfo, _T("\n"));
     while( tkz.HasMoreTokens() )
     {
@@ -69,33 +69,33 @@ void androidGetDeviceName()
             }
         }
     }
-    
+
 }
 #endif
 
 bool IsDongleAvailable()
 {
-#ifndef __OCPN__ANDROID__    
+#ifndef __OCPN__ANDROID__
 ///
-#if 0    
+#if 0
     wxString cmdls = _T("ls -la /Applications/OpenCPN.app/Contents/PlugIns/oernc_pi");
 
-    wxArrayString lsret_array, lserr_array;      
+    wxArrayString lsret_array, lserr_array;
     wxExecute(cmdls, lsret_array, lserr_array );
-            
+
     wxLogMessage(_T("ls results:"));
     for(unsigned int i=0 ; i < lsret_array.GetCount() ; i++){
         wxString line = lsret_array[i];
         wxLogMessage(line);
     }
-#endif 
-/// 
+#endif
+///
     wxString cmd = g_sencutil_bin;
     cmd += _T(" -s ");                  // Available?
 
-    wxArrayString ret_array, err_array;      
+    wxArrayString ret_array, err_array;
     wxExecute(cmd, ret_array, err_array );
-            
+
     wxLogMessage(_T("oexserverd results:"));
     for(unsigned int i=0 ; i < ret_array.GetCount() ; i++){
         wxString line = ret_array[i];
@@ -117,42 +117,42 @@ bool IsDongleAvailable()
     }
 
 #endif
-    
+
     return false;
 }
 
 unsigned int GetDongleSN()
 {
     unsigned int rv = 0;
- 
-#ifndef __OCPN__ANDROID__    
+
+#ifndef __OCPN__ANDROID__
     wxString cmd = g_sencutil_bin;
     cmd += _T(" -t ");                  // SN
 
-    wxArrayString ret_array;      
+    wxArrayString ret_array;
     wxExecute(cmd, ret_array, ret_array );
-            
+
     for(unsigned int i=0 ; i < ret_array.GetCount() ; i++){
         wxString line = ret_array[i];
         long sn;
         line.ToLong(&sn, 10);
         rv = sn;
     }
-#endif    
+#endif
     return rv;
 }
-    
+
 wxString GetServerVersionString()
 {
     wxString ver;
-#ifndef __OCPN__ANDROID__    
-    
+#ifndef __OCPN__ANDROID__
+
     wxString cmd = g_sencutil_bin;
     cmd += _T(" -a ");                  // Version
 
-    wxArrayString ret_array;      
+    wxArrayString ret_array;
     wxExecute(cmd, ret_array, ret_array );
-            
+
     for(unsigned int i=0 ; i < ret_array.GetCount() ; i++){
         wxString line = ret_array[i];
         if(line.Length() > 2){
@@ -162,22 +162,22 @@ wxString GetServerVersionString()
     }
 #else
     ver = _T("1.0");
-#endif    
+#endif
     return ver;
 }
 
 wxString getExpDate( wxString rkey)
 {
     wxString ret;
-#ifndef __OCPN__ANDROID__    
-    
+#ifndef __OCPN__ANDROID__
+
     wxString cmd = g_sencutil_bin;
     cmd += _T(" -v ");                  // Expiry days
     cmd += rkey;
 
-    wxArrayString ret_array;      
+    wxArrayString ret_array;
     wxExecute(cmd, ret_array, ret_array );
-            
+
     for(unsigned int i=0 ; i < ret_array.GetCount() ; i++){
         wxString line = ret_array[i];
         if(line.Length() > 2){
@@ -187,31 +187,31 @@ wxString getExpDate( wxString rkey)
     }
 #else
     ret = _T("9999 60");
-#endif    
+#endif
     return ret;
 
 }
 
 wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extra_info)
 {
-#ifndef __OCPN__ANDROID__    
-            
+#ifndef __OCPN__ANDROID__
+
             wxString msg1;
             wxString fpr_file;
             wxString fpr_dir = *GetpPrivateApplicationDataLocation(); //GetWritableDocumentsDir();
-            
+
 #ifdef __WXMSW__
-            
+
             //  On XP, we simply use the root directory, since any other directory may be hidden
             int major, minor;
             ::wxGetOsVersion( &major, &minor );
             if( (major == 5) && (minor == 1) )
                 fpr_dir = _T("C:\\");
-#endif        
-            
+#endif
+
             if( fpr_dir.Last() != wxFileName::GetPathSeparator() )
                 fpr_dir += wxFileName::GetPathSeparator();
-            
+
             wxString cmd = g_sencutil_bin;
             if(extra_info.Length()){
                 cmd += " -y ";
@@ -219,38 +219,39 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                 cmd += extra_info;
                 cmd += "\'";
             }
-            
+
             if(bSGLock)
                 cmd += _T(" -k ");                  // Make SGLock fingerprint
             else
                 cmd += _T(" -g ");                  // Make fingerprint
-            
+
 #ifndef __WXMSW__
             cmd += _T("\"");
             cmd += fpr_dir;
-            
+
             //cmd += _T("my fpr/");             // testing
-            
+
             //            wxString tst_cedilla = wxString::Format(_T("my fpr copy %cCedilla/"), 0x00E7);       // testing French cedilla
             //            cmd += tst_cedilla;            // testing
-            
+
             cmd += _T("\"");
 #else
-            cmd += wxString('\"'); 
+            cmd += wxString('\"');
             cmd += fpr_dir;
-            
+            cmd += _T("\"");
+
             //            cmd += _T("my fpr\\");            // testing spaces in path
-            
+
             //            wxString tst_cedilla = wxString::Format(_T("my%c\\"), 0x00E7);       // testing French cedilla
             //            cmd += tst_cedilla;            // testing
-#endif            
+#endif
             wxLogMessage(_T("Create FPR command: ") + cmd);
-            
+
             ::wxBeginBusyCursor();
-            
-            wxArrayString ret_array, err_array;      
+
+            wxArrayString ret_array, err_array;
             wxExecute(cmd, ret_array, err_array );
-            
+
             ::wxEndBusyCursor();
             wxLogMessage(_T("Create FPR oeaserverd results:"));
 
@@ -265,9 +266,9 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                 if(line.Upper().Find(_T("FPR")) != wxNOT_FOUND){
                     fpr_file = line.AfterFirst(':');
                 }
-                
+
             }
-            
+
             if(err_array.GetCount()){
                 wxLogMessage(_T("Create FPR oeaserverd execution error:"));
                 for(unsigned int i=0 ; i < err_array.GetCount() ; i++){
@@ -282,75 +283,75 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                     return fpr_file;
                 }
             }
-            
-            
+
+
             bool berror = false;
-            
+
             if( bCopyToDesktop && !berr && fpr_file.Length()){
-                
+
                 bool bcopy = false;
                 wxString sdesktop_path;
-                
+
 #ifdef __WXMSW__
                 TCHAR desktop_path[MAX_PATH*2] = { 0 };
                 bool bpathGood = false;
                 HRESULT  hr;
                 HANDLE ProcToken = NULL;
                 OpenProcessToken( GetCurrentProcess(), TOKEN_READ, &ProcToken );
-                
+
                 hr = SHGetFolderPath( NULL,  CSIDL_DESKTOPDIRECTORY, ProcToken, 0, desktop_path);
-                if (SUCCEEDED(hr))    
+                if (SUCCEEDED(hr))
                     bpathGood = true;
-                
+
                 CloseHandle( ProcToken );
-                
+
                 //                wchar_t *desktop_path = 0;
                 //                bool bpathGood = false;
-                
+
                 //               if( (major == 5) && (minor == 1) ){             //XP
                 //                    if(S_OK == SHGetFolderPath( (HWND)0,  CSIDL_DESKTOPDIRECTORY, NULL, SHGFP_TYPE_CURRENT, desktop_path))
                 //                        bpathGood = true;
-                
-                
+
+
                 //                 }
                 //                 else{
                     //                     if(S_OK == SHGetKnownFolderPath( FOLDERID_Desktop, 0, 0, &desktop_path))
                 //                         bpathGood = true;
                 //                 }
-                
-                
+
+
                 if(bpathGood){
-                    
+
                     char str[128];
                     wcstombs(str, desktop_path, 128);
                     wxString desktop_fpr(str, wxConvAuto());
-                    
+
                     sdesktop_path = desktop_fpr;
                     if( desktop_fpr.Last() != wxFileName::GetPathSeparator() )
                         desktop_fpr += wxFileName::GetPathSeparator();
-                    
+
                     wxFileName fn(fpr_file);
                     wxString desktop_fpr_file = desktop_fpr + fn.GetFullName();
-                    
-                    
+
+
                     wxString exe = _T("xcopy");
                     wxString parms = fpr_file.Trim() + _T(" ") + wxString('\"') + desktop_fpr + wxString('\"');
                     wxLogMessage(_T("FPR copy command: ") + exe + _T(" ") + parms);
-                    
+
                     const wchar_t *wexe = exe.wc_str(wxConvUTF8);
                     const wchar_t *wparms = parms.wc_str(wxConvUTF8);
-                    
+
                     if( (major == 5) && (minor == 1) ){             //XP
                         // For some reason, this does not work...
                         //8:43:13 PM: Error: Failed to copy the file 'C:\oc01W_1481247791.fpr' to '"C:\Documents and Settings\dsr\Desktop\oc01W_1481247791.fpr"'
                         //                (error 123: the filename, directory name, or volume label syntax is incorrect.)
                         //8:43:15 PM: oesenc fpr file created as: C:\oc01W_1481247791.fpr
-                        
+
                         bcopy = wxCopyFile(fpr_file.Trim(false), _T("\"") + desktop_fpr_file + _T("\""));
                     }
                     else{
                         ::wxBeginBusyCursor();
-                        
+
                         // Launch oexserverd as admin
                         SHELLEXECUTEINFO sei = { sizeof(sei) };
                         sei.lpVerb = L"runas";
@@ -359,7 +360,7 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                         sei.lpParameters = wparms;
                         sei.nShow = SW_SHOWMINIMIZED;
                         sei.fMask = SEE_MASK_NOASYNC;
-                        
+
                         if (!ShellExecuteEx(&sei))
                         {
                             DWORD dwError = GetLastError();
@@ -372,10 +373,10 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                         }
                         else
                             bcopy = true;
-                        
+
                         ::wxEndBusyCursor();
-                        
-                    }  
+
+                    }
                 }
 #endif            // MSW
 
@@ -383,17 +384,17 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
                 wxFileName fn(fpr_file);
                 wxString desktop_fpr_path = ::wxGetHomeDir() + wxFileName::GetPathSeparator() +
                 _T("Desktop") + wxFileName::GetPathSeparator() + fn.GetFullName();
-                
+
                 bcopy =  ::wxCopyFile(fpr_file.Trim(false), desktop_fpr_path);
                 sdesktop_path = desktop_fpr_path;
                 msg1 += _T("\n\n OSX ");
 #endif
-                
-                
+
+
                 wxLogMessage(_T("oeRNC fpr file created as: ") + fpr_file);
                 if(bCopyToDesktop && bcopy)
                     wxLogMessage(_T("oeRNC fpr file created in desktop folder: ") + sdesktop_path);
-                
+
                 if(bcopy)
                     bCopyOK = true;
         }
@@ -405,12 +406,12 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
             }
             berror = true;
         }
-        
+
         if(berror)
             return _T("");
         else
             return fpr_file;
-        
+
 #else   // android
 
         // Get XFPR from the oexserverd helper utility.
@@ -424,13 +425,13 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
         dataDir += _T("cache/");
 
         wxString rootDir = fn.GetPath(wxPATH_GET_SEPARATOR);
-        
+
         //  Set up the parameter passed to runtime environment as LD_LIBRARY_PATH
         // This will be {dir of g_server_bin}
         wxFileName fnl(cmd);
 //        wxString libDir = fnl.GetPath();
         wxString libDir = fnl.GetPath(wxPATH_GET_SEPARATOR) + _T("lib");
-      
+
         wxLogMessage(_T("oernc_pi: Getting XFPR: Starting: ") + cmd );
         wxLogMessage(_T("oernc_pi: Getting XFPR: Parms: ") + rootDir + _T(" ") + dataDir + _T(" ") + libDir );
 
@@ -439,7 +440,7 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
         wxLogMessage(_T("oernc_pi: Start Result: ") + result);
 
         bool berror = true;            //TODO
-        
+
         // Find the file...
         wxArrayString files;
         wxString lastFile = _T("NOT_FOUND");
@@ -463,7 +464,7 @@ wxString getFPR( bool bCopyToDesktop, bool &bCopyOK, bool bSGLock, wxString extr
         else
             return _T("");
 
-#endif        
+#endif
 }
 
 #ifdef __OCPN__ANDROID__
@@ -471,7 +472,7 @@ wxString androidGetSystemName()
 {
     wxString detectedSystemName;
     wxString deviceInfo = callActivityMethod_vs("getDeviceInfo");
-    
+
     wxStringTokenizer tkz(deviceInfo, _T("\n"));
     while( tkz.HasMoreTokens() )
     {
@@ -482,8 +483,8 @@ wxString androidGetSystemName()
         }
     }
     qDebug() << "systemName by deviceInfo: " << detectedSystemName.mb_str();
-    
+
     return detectedSystemName;
-    
+
 }
 #endif
