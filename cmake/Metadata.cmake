0dab0cc9c5243a7392ddbdd3d96bdef76502b34a
dsr <bdbcat@yahoo.com>
2022-12-12 13:33:54 -0500

    Revert prior commit.

--- cmake/Metadata.cmake
+++ cmake/Metadata.cmake
@@ -176,11 +176,11 @@ if ("${BUILD_TYPE}" STREQUAL "flatpak")
     set(pkg_target_arch "${pkg_target_arch}-${OCPN_WX_ABI}")
   endif ()
 elseif ("${plugin_target}" MATCHES "ubuntu|raspbian|debian|mingw")
-  set(pkg_target_arch "${plugin_target}-${_wx_abi}-${ARCH}")
+  set(pkg_target_arch "${plugin_target}-${ARCH}")
 else ()
   set(pkg_target_arch "${plugin_target}")
 endif ()
 
-message(STATUS "pkg_target_arch: ${pkg_target_arch}.")
+message(STATUS "pkg_target_arch: ${pkg_displayname}.")
 
 #cmake-format: on
