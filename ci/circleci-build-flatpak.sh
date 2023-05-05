ddcf4e30dc7787612315850fa11a17b6cbee148f
dsr <bdbcat@yahoo.com>
2023-05-04 23:17:59 -0400

    Force flatpak build to one processor core.

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -78,7 +78,7 @@ flatpak remote-add --user --if-not-exists \
 
 # Configure and build the plugin tarball and metadata.
 cmake -DCMAKE_BUILD_TYPE=Release -DOCPN_WX_ABI=wx32 ..
-make -j $(nproc) VERBOSE=1 flatpak
+make -j 1 VERBOSE=1 flatpak
 
 # Restore permissions and owner in build tree.
 if [ -d /ci-source ]; then sudo chown --reference=/ci-source -R . ../cache; fi
