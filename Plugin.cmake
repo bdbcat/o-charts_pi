77cb701996920d19544ebbe5e87fec5933cb31e4
dsr <bdbcat@yahoo.com>
2023-06-23 21:56:34 -0400

    Support O5.6.2 core by adding explicit GLEW dependency

--- Plugin.cmake
+++ Plugin.cmake
@@ -164,6 +164,13 @@ macro(add_plugin_libraries)
   add_subdirectory(libs/pugixml)
   target_link_libraries(${PACKAGE_NAME} ocpn::pugixml)
 
+# For OCPN 5.6.2, which does not use GLES in core, we need to add it here.
+if (UNIX AND NOT APPLE AND NOT QT_ANDROID)   # linux
+  if(OCPN_BUILD_USE_GLEW)
+    target_link_libraries(${PACKAGE_NAME} "GLEW")
+  endif(OCPN_BUILD_USE_GLEW)
+endif(UNIX AND NOT APPLE)
+
 #if (MSVC)
 #  add_subdirectory("libs/WindowsHeaders")
 #  target_link_libraries(${PACKAGE_NAME} _windows_headers)
